5a5851c425c9db46644312444923cfb007d906a7
xmalachi <angelmalachi5@gmail.com>
2021-06-25 14:04:54 +0100

    first commit

---
+++ menu.js
@@ -0,0 +1,23 @@
+/*Hamburger Animation*/
+const mainMenu = document.querySelector(`.mainMenu`);
+const closeMenu = document.querySelector(`.closeMenu`);
+const openMenu = document.querySelector(`.openMenu`);
+const openservice = document.querySelector(`.openservice`);
+const opencontact = document.querySelector(`.opencontact`);
+
+
+openMenu.addEventListener(`click`,show);
+closeMenu.addEventListener(`click`,close);
+openservice.addEventListener(`click`,close);
+opencontact.addEventListener(`click`,close);
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+
+}
+function close() {
+    mainMenu.style.top = '-150%';
+
+}
+
